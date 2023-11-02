7ae496db51c16798def6625e5397d22c3b399080
Everson Soares <eversonv4@gmail.com>
2023-11-02 14:26:44 -0300

    feat: created seed to user and adress

--- database/seeders/CreateUserAndAddressSeed.php
+++ database/seeders/CreateUserAndAddressSeed.php
@@ -4,14 +4,25 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUserAndAddressSeed extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    //
+    DB::table("users")->insert([
+      "name" => "Everson",
+      "email" => "everson@email.com",
+      "password" => Hash::make("senhadificil"),
+    ]);
+
+    DB::table("addresses")->insert([
+      "address" => "Rua qualquer numero qualquer um",
+    ]);
+  }
 }
